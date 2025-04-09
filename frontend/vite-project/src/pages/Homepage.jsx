ea72c5ccfc8b9ec9b6ddaf251b92272cebba2ed3
Yuno Kim <unokim1547@gmail.com>
2025-04-09 15:27:14 +0900

    home page

--- frontend/vite-project/src/pages/Homepage.jsx
+++ frontend/vite-project/src/pages/Homepage.jsx
@@ -1,8 +1,11 @@
+import logo from "../assets/logo.png"
+
 const HomePage = () => {
   return (
     <>
-    <div>여긴 홈뷰</div>
-    <div>팀장이 홈뷰에 추가한 내용</div>
+    {/* <div className = {}>여긴 홈뷰</div> */}
+    <img src={logo} alt="" />
+    {/* <div>팀장이 홈뷰에 추가한 내용</div> */}
     </>
   )
 }
