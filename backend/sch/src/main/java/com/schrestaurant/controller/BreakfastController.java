c15be32b29d6b5272d72aaea03aaf21e92c099fa
LDH <shcass4651@naver.com>
2025-05-03 17:08:36 +0900

    add CrossOrigin

--- backend/sch/src/main/java/com/schrestaurant/controller/BreakfastController.java
+++ backend/sch/src/main/java/com/schrestaurant/controller/BreakfastController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/payment")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class BreakfastController {
 
     private final BreakfastService breakfastService;
