8c8c7087e768637b22ca82e486fc631ad92b5ba1
Yuno Kim <unokim1547@gmail.com>
2025-06-30 04:15:30 +0900

    styling complete

--- backend/sch/src/main/frontend/vite-project/src/pages/EnqueuePage.jsx
+++ backend/sch/src/main/frontend/vite-project/src/pages/EnqueuePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { startTransition, useState } from "react";
 import ricepan from "../../../img/천밥.png";
 import Button from "../components/Button/Button";
 import style from "./EnqueuePage.module.css"
@@ -24,13 +24,33 @@ const EnqueuePage = () => {
     }
     return (
         <>
-            <h1>번호표뽑기</h1>
-            <div className={style.name}>천원의 아침밥[번호표]</div>
-            <div>
-                <img src={ricepan} alt="" className={style.img}/>
-            </div>
-            <div>
+            <h1>현재 대기</h1>
+            {/* <div className={style.content}>
+                <h2>20명</h2>
                 <Button text="번호표뽑기" handler={NumberTicketHandler} />
+            </div> */}
+            <div className={style.wrapper}>
+                <div className={style["cnt-div"]}>
+                    <div className={style.stock}>
+                        <span className={style.cntH}>잔여 수량</span>
+                        <div>
+                            <span className={style.cntV}>{`${20} / 100`}</span>
+                            <span className={style.cntH}>개</span>
+                        </div>
+                        {/* <span className = {style.cntV}>{`${quantity ? quantity: 50} / 100 개`}</span> */}
+                        {/* <span className = {style.cntV}>{`${quantity ? quantity: 50} / 100`}</span> */}
+                    </div>
+                    <div className={style.wait}>
+                        <span className={style.cntH}>대기 인원 수</span>
+                        <div>
+                            <span className={style.cntV}>{15}</span>
+                            <span className={style.cntH}>명</span>
+                        </div>
+                        {/* <span className = {style.cntV}>{`${waitCnt ? waitCnt : 5} 명`}</span> */}
+                        {/* <span className = {style.cntV}>{`${waitCnt ? waitCnt : 5}`}</span> */}
+                    </div>
+                </div>
+                <div className={style["btn-div"]} onClick = {NumberTicketHandler}>줄서기</div>
             </div>
         </>
     )
